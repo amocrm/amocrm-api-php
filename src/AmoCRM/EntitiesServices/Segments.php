65be6a80abc3aac725dbb82277e47a8e16608834
Nikita <nik1004@gmail.com>
2020-05-10 19:17:23 +0300

    segments

--- src/AmoCRM/EntitiesServices/Segments.php
+++ src/AmoCRM/EntitiesServices/Segments.php
@@ -2,7 +2,8 @@
 
 namespace AmoCRM\EntitiesServices;
 
-use AmoCRM\AmoCRM\Helpers\EntityTypesInterface;
+use AmoCRM\Filters\BaseEntityFilter;
+use AmoCRM\Helpers\EntityTypesInterface;
 use AmoCRM\Client\AmoCRMApiClient;
 use AmoCRM\Client\AmoCRMApiRequest;
 use AmoCRM\Collections\BaseApiCollection;
@@ -12,16 +13,43 @@ use AmoCRM\EntitiesServices\Traits\PageMethodsTrait;
 use AmoCRM\Models\BaseApiModel;
 use AmoCRM\Models\Customers\Segments\SegmentModel;
 
+/**
+ * Class Segments
+ *
+ * @package AmoCRM\EntitiesServices
+ *
+ * @method SegmentModel getOne($id, array $with = []) : ?SegmentModel
+ * @method SegmentsCollection get(BaseEntityFilter $filter = null, array $with = []) : ?SegmentsCollection
+ * @method SegmentModel addOne(BaseApiModel $model) : SegmentModel
+ * @method SegmentsCollection add(BaseApiCollection $collection) : SegmentsCollection
+ * @method SegmentModel updateOne(BaseApiModel $apiModel) : SegmentModel
+ * @method SegmentsCollection update(BaseApiCollection $collection) : SegmentsCollection
+ * @method SegmentModel syncOne(BaseApiModel $apiModel, $with = []) : SegmentModel
+ */
 class Segments extends BaseEntity implements HasPageMethodsInterface
 {
     use PageMethodsTrait;
 
+    /**
+     * @var string
+     */
     protected $method = 'api/v' . AmoCRMApiClient::API_VERSION . '/' . EntityTypesInterface::CUSTOMERS . '/' . EntityTypesInterface::CUSTOMERS_SEGMENTS;
 
+    /**
+     * @var string
+     */
     protected $collectionClass = SegmentsCollection::class;
 
+    /**
+     * @var string
+     */
     protected $itemClass = SegmentModel::class;
 
+    /**
+     * @param array $response
+     *
+     * @return array
+     */
     protected function getEntitiesFromResponse(array $response): array
     {
         $entities = [];
@@ -36,6 +64,7 @@ class Segments extends BaseEntity implements HasPageMethodsInterface
     /**
      * @param BaseApiModel $model
      * @param array $response
+     *
      * @return BaseApiModel
      */
     protected function processUpdateOne(BaseApiModel $model, array $response): BaseApiModel
@@ -48,6 +77,7 @@ class Segments extends BaseEntity implements HasPageMethodsInterface
     /**
      * @param BaseApiCollection $collection
      * @param array $response
+     *
      * @return BaseApiCollection
      */
     protected function processUpdate(BaseApiCollection $collection, array $response): BaseApiCollection
@@ -58,6 +88,7 @@ class Segments extends BaseEntity implements HasPageMethodsInterface
     /**
      * @param BaseApiCollection $collection
      * @param array $response
+     *
      * @return BaseApiCollection
      */
     protected function processAdd(BaseApiCollection $collection, array $response): BaseApiCollection
@@ -65,6 +96,12 @@ class Segments extends BaseEntity implements HasPageMethodsInterface
         return $this->processAction($collection, $response);
     }
 
+    /**
+     * @param BaseApiCollection $collection
+     * @param array $response
+     *
+     * @return BaseApiCollection
+     */
     protected function processAction(BaseApiCollection $collection, array $response): BaseApiCollection
     {
         $entities = $this->getEntitiesFromResponse($response);
@@ -81,18 +118,21 @@ class Segments extends BaseEntity implements HasPageMethodsInterface
     }
 
     /**
-     * @param BaseApiModel $apiModel
+     * @param BaseApiModel|SegmentModel $apiModel
      * @param array $entity
      */
     protected function processModelAction(BaseApiModel $apiModel, array $entity): void
     {
-        //todo
         if (isset($entity['id'])) {
             $apiModel->setId($entity['id']);
         }
 
         if (isset($entity['updated_at'])) {
             $apiModel->setUpdatedAt($entity['updated_at']);
+        }
+
+        if (isset($entity['color'])) {
+            $apiModel->setColor($entity['color']);
         }
     }
 }
