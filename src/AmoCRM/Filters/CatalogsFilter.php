b7045f703677bb558192ed366145653471fb9900
Aydarkhan Tsyrenzhapov <aydarkhan@icloud.com>
2020-08-31 14:34:37 +0300

    catalogs filter by type

---
+++ src/AmoCRM/Filters/CatalogsFilter.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AmoCRM\AmoCRM\Filters;
+
+use AmoCRM\Filters\BaseEntityFilter;
+
+use function is_null;
+
+class CatalogsFilter extends BaseEntityFilter
+{
+    /** @var null|string */
+    private $type = null;
+
+    /**
+     * @param null|string $type
+     *
+     * @return $this
+     */
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function buildFilter(): array
+    {
+        $filter = [];
+
+        if (!is_null($this->getType())) {
+            $filter['type'] = $this->getType();
+        }
+
+        return $filter;
+    }
+}
