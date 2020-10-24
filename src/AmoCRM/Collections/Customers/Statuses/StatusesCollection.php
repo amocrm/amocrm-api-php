0563fc6f8e97e81b987f5672865f653872f32485
dkorolev <dkorolev@team.amocrm.com>
2020-10-24 20:19:06 +0300

    (fix) wrong status model

--- src/AmoCRM/Collections/Customers/Statuses/StatusesCollection.php
+++ src/AmoCRM/Collections/Customers/Statuses/StatusesCollection.php
@@ -3,7 +3,7 @@
 namespace AmoCRM\Collections\Customers\Statuses;
 
 use AmoCRM\Collections\BaseApiCollection;
-use AmoCRM\Models\Leads\Pipelines\Statuses\StatusModel;
+use AmoCRM\Models\Customers\Statuses\StatusModel;
 
 class StatusesCollection extends BaseApiCollection
 {
