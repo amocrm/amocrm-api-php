d5e757e8ad5047f60449527de81fd97eea799aa8
Nikita <nik1004@gmail.com>
2021-05-14 13:31:50 +0300

    fix after merge

--- src/AmoCRM/Client/AmoCRMApiClient.php
+++ src/AmoCRM/Client/AmoCRMApiClient.php
@@ -512,7 +512,7 @@ class AmoCRMApiClient
     /**
      * Метод вернет объект транзакций
      *
-     * @deprecated Будет удалено в релизе 0.8 (Ориентировачно осень-зима 2021)
+     * @deprecated Скорее всего будет удалено в релизе 0.8 (Ориентировачно осень-зима 2021)
      * @return Transactions
      * @throws AmoCRMMissedTokenException
      */
