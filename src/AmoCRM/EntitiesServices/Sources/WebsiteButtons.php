fdc62000560adfc530538c113cd78baf9eabc0e8
egrigorev <egrigorev@team.amocrm.com>
2024-01-28 18:25:51 +0300

    1. Fix readme new service website buttons 2. Add unsupported method to website buttons service

--- src/AmoCRM/EntitiesServices/Sources/WebsiteButtons.php
+++ src/AmoCRM/EntitiesServices/Sources/WebsiteButtons.php
@@ -8,13 +8,16 @@ use AmoCRM\Collections\Sources\WebsiteButtonsCollection;
 use AmoCRM\Models\Sources\WebsiteButtonModel;
 use AmoCRM\Client\AmoCRMApiClient;
 use AmoCRM\Client\AmoCRMApiRequest;
+use AmoCRM\Collections\BaseApiCollection;
 use AmoCRM\EntitiesServices\BaseEntity;
 use AmoCRM\EntitiesServices\Traits\PageMethodsTrait;
 use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Exceptions\AmoCRMApiNoContentException;
 use AmoCRM\Exceptions\AmoCRMoAuthApiException;
+use AmoCRM\Exceptions\NotAvailableForActionException;
 use AmoCRM\Filters\BaseEntityFilter;
 use AmoCRM\Helpers\EntityTypesInterface;
+use AmoCRM\Models\BaseApiModel;
 use AmoCRM\Models\Sources\WebsiteButtonCreateRequestModel;
 use AmoCRM\Models\Sources\WebsiteButtonCreateResponseModel;
 use AmoCRM\Models\Sources\WebsiteButtonUpdateRequestModel;
@@ -85,5 +88,61 @@ class WebsiteButtons extends BaseEntity
         );
 
         return WebsiteButtonModel::fromArray($response);
+    }
+
+    /**
+     * @param BaseApiModel $model
+     *
+     * @return BaseApiModel
+     * @throws NotAvailableForActionException
+     */
+    public function addOne(BaseApiModel $model): BaseApiModel
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiCollection $collection
+     *
+     * @return BaseApiCollection
+     * @throws NotAvailableForActionException
+     */
+    public function add(BaseApiCollection $collection): BaseApiCollection
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiCollection $collection
+     *
+     * @return BaseApiCollection
+     * @throws NotAvailableForActionException
+     */
+    public function update(BaseApiCollection $collection): BaseApiCollection
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiModel $apiModel
+     *
+     * @return BaseApiModel
+     * @throws NotAvailableForActionException
+     */
+    public function updateOne(BaseApiModel $apiModel): BaseApiModel
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiModel $apiModel
+     * @param array $with
+     *
+     * @return BaseApiModel
+     * @throws NotAvailableForActionException
+     */
+    public function syncOne(BaseApiModel $apiModel, $with = []): BaseApiModel
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
     }
 }
