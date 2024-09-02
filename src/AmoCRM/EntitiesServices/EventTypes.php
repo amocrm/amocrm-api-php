ff1e349b31a3dda6c3120d364fdb88304aa69fcd
MaksimKutishchev <max@amopro.ru>
2024-09-02 22:44:54 +0400

    Feature: add event types

---
+++ src/AmoCRM/EntitiesServices/EventTypes.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace AmoCRM\EntitiesServices;
+
+use AmoCRM\Client\AmoCRMApiClient;
+use AmoCRM\Client\AmoCRMApiRequest;
+use AmoCRM\Collections\BaseApiCollection;
+use AmoCRM\Collections\EventsCollections;
+use AmoCRM\Collections\EventTypesCollections;
+use AmoCRM\Exceptions\NotAvailableForActionException;
+use AmoCRM\Filters\BaseEntityFilter;
+use AmoCRM\Helpers\EntityTypesInterface;
+use AmoCRM\Models\BaseApiModel;
+use AmoCRM\Models\EventTypeModel;
+
+/**
+ * Class EventTypes
+ *
+ * @package AmoCRM\EntitiesServices
+ *
+ * @method null|EventsCollections get(BaseEntityFilter $filter = null, array $with = [])
+ */
+class EventTypes extends BaseEntity
+{
+    /**
+     * @var string
+     */
+    protected $method = 'api/v' . AmoCRMApiClient::API_VERSION . '/' . EntityTypesInterface::EVENTS . '/types';
+
+    /**
+     * @var string
+     */
+    protected $collectionClass = EventTypesCollections::class;
+
+    /**
+     * @var string
+     */
+    public const ITEM_CLASS = EventTypeModel::class;
+
+    /**
+     * @param array $response
+     *
+     * @return array
+     */
+    protected function getEntitiesFromResponse(array $response): array
+    {
+        $entities = $response[AmoCRMApiRequest::EMBEDDED][EntityTypesInterface::EVENTS_TYPES] ?? [];
+        
+        return $entities;
+    }
+
+    /**
+     * @param BaseApiModel $model
+     *
+     * @return BaseApiModel
+     * @throws NotAvailableForActionException
+     */
+    public function addOne(BaseApiModel $model): BaseApiModel
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiCollection $collection
+     *
+     * @return BaseApiCollection
+     * @throws NotAvailableForActionException
+     */
+    public function add(BaseApiCollection $collection): BaseApiCollection
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiCollection $collection
+     *
+     * @return BaseApiCollection
+     * @throws NotAvailableForActionException
+     */
+    public function update(BaseApiCollection $collection): BaseApiCollection
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiModel $apiModel
+     *
+     * @return BaseApiModel
+     * @throws NotAvailableForActionException
+     */
+    public function updateOne(BaseApiModel $apiModel): BaseApiModel
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+
+    /**
+     * @param BaseApiModel $apiModel
+     * @param array $with
+     *
+     * @return BaseApiModel
+     * @throws NotAvailableForActionException
+     */
+    public function syncOne(BaseApiModel $apiModel, $with = []): BaseApiModel
+    {
+        throw new NotAvailableForActionException('Method not available for this entity');
+    }
+}
